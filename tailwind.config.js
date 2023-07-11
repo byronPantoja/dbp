e220d12d6423f2d27305a2ce02a2df9b9ec23bc8
byronpantoja <byronpantoja@gmail.com>
2023-07-11 18:28:23 +0800

    adding presets and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          main: "#ff8482",
+          light: "#ff9d9b",
+          lighter: "#ffdada",
+          dark: "#e67775",
+          darker: "#994f4e",
+        },
+        blue: {
+          main: "#403b8f",
+          light: "#534f9a",
+          lighter: "#8c89bc",
+          dark: "#332f72",
+          darker: "#201e48",
+        },
+        grey: {
+          main: "#f2f2f2",
+          secondary: "#cacaca",
+          dark: "#656565",
+          darker: "#4c4c4c",
+        },
+        fill: {
+          mainbg: "#fcfcfc",
+          black: "#191919",
+          white: "#fefefe",
+        },
+      },
+      fontFamily: {
+        sans: ["DM Sans", "DM-sans, sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
 };
