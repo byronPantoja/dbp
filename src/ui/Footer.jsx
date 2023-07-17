3765985a295c96e5eb6e52449b3b7c9dee5036f1
byronpantoja <byronpantoja@gmail.com>
2023-07-17 12:09:47 +0800

    adding a link to /signin using my copyright text

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,11 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 function Footer() {
   return (
     <footer className="bg-white">
       <div className="mx-auto flex max-w-7xl justify-center px-6 py-8 md:flex md:items-center lg:px-8">
         <div className="space-x-6">
-          <p className="text-center text-xs text-gray-500">
+          <NavLink to="/signin" className="text-center text-xs text-gray-500">
             &copy; 2023 byronpantoja.com. All rights reserved.
-          </p>
+          </NavLink>
         </div>
       </div>
     </footer>
