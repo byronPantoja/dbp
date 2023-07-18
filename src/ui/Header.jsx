1209bf2717af1b72b2f3b572803439ebd0189ff8
byronpantoja <byronpantoja@gmail.com>
2023-07-18 22:32:21 +0800

    fixing mobile menu not closing on navlink click

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -66,7 +66,7 @@ function Header() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <NavLink key={item.page} to={item.path}>
+                  <NavLink key={item.page} to={item.path} onClick={() => setMobileMenuOpen(false)} >
                     <span className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-fill-black">
                       {item.page}
                     </span>
